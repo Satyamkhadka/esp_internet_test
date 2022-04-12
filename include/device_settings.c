a14e5a302fb2bd0104305cc87558db77e9f6f862
s4aty4m <satyam.khadka123@gmail.com>
2022-04-12 10:24:39 +0545

    new

---
+++ include/device_settings.c
@@ -0,0 +1,17 @@
+
+
+
+#include "device_settings.h"
+
+char * apn;
+bool debug;
+int log_level;
+
+esp_err_t device_init(void){
+
+}
+
+
+esp_err_t set_new_apn(char * apn){
+
+}
---
+++ include/device_settings.h
@@ -0,0 +1,5 @@
+#include "esp_err.h"
+#include "stdbool.h"
+char *apn;
+bool debug;
+int log_level;
